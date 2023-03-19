a3275ea441d20dc3e9ddcbc77700bbd71b8e5ad4
SusanLiu0405 <15301609750@163.com>
2023-03-19 00:17:35 -0400

    testcase passed

--- src/cisco/digit_add_up_toY/Main.java
+++ src/cisco/digit_add_up_toY/Main.java
@@ -5,38 +5,68 @@ import java.util.*;
 // 例如：X=20， Y=5
 // 符合要求的数字：5，14
 public class Main {
-    public static void matchXAndY(int X, int Y) {
-        // find the digits sum of Y
-        int length = String.valueOf(X).length();
-        System.out.println(length);
-        int count = 0;
-        for (int i = 1; i <= length; i++) {
-            count+= getNumber(i, 0, 0, Y, X);
+    public static void main(String[] args){
+        int X = 20;
+        int Y = 5;
+        find_valid_nums(X,Y);
+    }
+    public static int sum_of_digits(int num){
+        int sum = 0;
+        while (num > 0){
+            int unit = num % 10;
+            sum +=unit;
+            num /=10;
         }
-        if (count == 0) {
-            System.out.println(-1);
+        return sum;
+    }
+
+    public static void find_valid_nums(int upper_bound, int unit_sum) {
+        int count = 0;
+        for(int i = 0; i < upper_bound; i++) {
+            if (sum_of_digits(i) == unit_sum) {
+                System.out.println(i);
+                count++;
+            }
+        }
+        if(count == 0) {
+            System.out.println("no numbers could meet the requirements");
         } else {
-            System.out.println(count);
+            System.out.println("there are " + count + " valid integers");
         }
+
     }
-    private static int getNumber(int index, int num,
-                                 int result, int target, int bound) {
-        if (num > bound) return 0;
-        if (index == 0) {
-            if (result == target) return 1;
-            else return 0;
-        }
-        int count = 0;
-        for (int i = 1; i < 10; i++) {
-            count += getNumber(index - 1, num * 10 + i,
-                    result + i, target, bound);
-        }
-        return count;
-    }
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int X = scanner.nextInt();
-        int Y = scanner.nextInt();
-        matchXAndY(X, Y);
-    }
+//    public static void matchXAndY(int X, int Y) {
+//        // find the digits sum of Y
+//        int length = String.valueOf(X).length();
+//        System.out.println(length);
+//        int count = 0;
+//        for (int i = 1; i <= length; i++) {
+//            count+= getNumber(i, 0, 0, Y, X);
+//        }
+//        if (count == 0) {
+//            System.out.println(-1);
+//        } else {
+//            System.out.println(count);
+//        }
+//    }
+//    private static int getNumber(int index, int num,
+//                                 int result, int target, int bound) {
+//        if (num > bound) return 0;
+//        if (index == 0) {
+//            if (result == target) return 1;
+//            else return 0;
+//        }
+//        int count = 0;
+//        for (int i = 1; i < 10; i++) {
+//            count += getNumber(index - 1, num * 10 + i,
+//                    result + i, target, bound);
+//        }
+//        return count;
+//    }
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        int X = scanner.nextInt();
+//        int Y = scanner.nextInt();
+//        matchXAndY(X, Y);
+//    }
 }
