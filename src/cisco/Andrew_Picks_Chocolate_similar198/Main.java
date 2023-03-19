9ba3eff8e316d88e88fc1fe82e04c3c5b62e4224
SusanLiu0405 <15301609750@163.com>
2023-03-18 23:03:50 -0400

    testcase passed, using dp

--- src/cisco/Andrew_Picks_Chocolate_similar198/Main.java
+++ src/cisco/Andrew_Picks_Chocolate_similar198/Main.java
@@ -1,39 +1,45 @@
 package cisco.Andrew_Picks_Chocolate_similar198;
-import java.util.*;
+
 //  There is a set of N jars containing chocolates. Some of them may be empty.
 //  Determine the maximum number of chocolates Andrew can pick from the jars
 //        given that he cannot pick jars next to each other
 //  Testcase fail: when only 1 input
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int length = scanner.nextInt();
-        int[] nums = new int[length];
-        for (int i = 0; i < length; i++) {
-            nums[i] = scanner.nextInt();
+    // Function to calculate the maximum chocolate numbers
+    static int maxChocolates(int chocolate_jar[], int n)
+    {
+        // corner case
+        if (n < 0) {
+            return 0;
         }
-        int result = rob(nums);
-        System.out.println(result);
+        if (n == 0) {
+            return chocolate_jar[0];
+        }
+
+        // if current element is pick then previous cannot
+        // be picked
+        int pick = chocolate_jar[n] + maxChocolates(chocolate_jar, n - 2);
+
+        // if current element is not picked then previous
+        // element is picked
+        int notPick = maxChocolates(chocolate_jar, n - 1);
+
+        // return max of picked and not picked
+        return Math.max(pick, notPick);
     }
 
-    public static int rob(int[] nums) {
-        if (nums == null || nums.length == 0) return 0;
-
-
-        if(nums.length == 1){
-            return nums[0];
-        }
-
-        if (nums.length == 1) return nums[0];
-// general case
-        int[] dp = new int[nums.length];
-// base case
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[1], dp[0]);
-        for (int i = 2; i < nums.length; i++) {
-            dp[i] = Math.max(dp[i - 1], nums[i] + dp[i - 2]);
-        }
-        return dp[nums.length - 1];
-
+    // test here
+    public static void main(String[] args) {
+        int chocolate_arr[] = {6, 7, 1, 3, 8, 2, 4};
+        int n = chocolate_arr.length;
+        System.out.println("Maximum chocolate possible : " + maxChocolates(chocolate_arr, n - 1));
+        // Scanner scanner = new.Scaner(System.in);
+        // int length = Scanner.nextInt();
+        // int[] nums = new int[length];
+        // for (int i = 0; i < length; i++) {
+        //      nums[i] = scanner.nextInt();
+        // }
+        // int result = maxChocolate(hval[], hval.length - 1);
+        // System.out.println(result);
     }
 }
