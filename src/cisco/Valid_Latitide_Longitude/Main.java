a8eafe2438f484d8fb7836b94e6dba808dd8915c
SusanLiu0405 <15301609750@163.com>
2023-03-21 01:04:53 -0400

    testcase not passed

--- src/cisco/Valid_Latitide_Longitude/Main.java
+++ src/cisco/Valid_Latitide_Longitude/Main.java
@@ -5,17 +5,26 @@ import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<String> input_list = new ArrayList<>();
+        String a = "(90, 180)";
+//        String b = "(+90,+180)";
+//        String c = "(90.,180)";
+//        String d = "(90.0,180.1)";
+//        String e = "(85S,95W)";
+        input_list.addAll(Arrays.asList(a));
+        System.out.println(latiLongPairs(input_list));
     }
-    public static void latiLongPairs(List<String> input) {
+    public static String latiLongPairs(List<String> input) {
         String regexLatLong = "\\([-+]?(([1-8]?[0-9])(\\.\\d+)?|90(\\.0+)?),[-+]?(([1-9]?[0-9]|1[0-7][0-9])(\\.\\d+)?|180(\\.0+)?)\\)";
         Pattern pattern = Pattern.compile(regexLatLong);
+        String flag="";
         for (String cur : input) {
             Matcher matcher = pattern.matcher(cur);
             if (matcher.find())
-                System.out.println("Valid");
+                flag="Valid";
             else
-                System.out.println("Invalid");
+                flag="Invalid";
         }
+        return flag;
     }
 }
