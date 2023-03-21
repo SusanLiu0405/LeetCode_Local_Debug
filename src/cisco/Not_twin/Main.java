a939b725dae0a5c45caa163fe8ed94e298049296
SusanLiu0405 <15301609750@163.com>
2023-03-20 23:09:24 -0400

    testcase passed

--- src/cisco/Not_twin/Main.java
+++ src/cisco/Not_twin/Main.java
@@ -1,20 +1,25 @@
 package cisco.Not_twin;
-
+//
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] test_array = {1,1,2,2,3,3,4};
+        System.out.println(firstSingle(test_array));
     }
 
     public static int firstSingle(int[] array) {
-// corner case
-        if (array == null || array.length == 0) return
-                -1;
-        if (array.length == 1) return array[0];
-// general case
+        // corner case
+        if (array == null || array.length == 0) {
+            return -1;
+        }
+        if (array.length == 1) {
+            return array[0];
+        }
+
+        // general case
         for (int i = 1; i < array.length; i++) {
-            if (array[i] != array[i - 1] && (i ==
-                    array.length - 1 || array[i] != array[i + 1]))
+            if (array[i] != array[i - 1] && (i == array.length - 1 || array[i] != array[i + 1])) {
                 return array[i];
+            }
         }
         return -1;
     }
