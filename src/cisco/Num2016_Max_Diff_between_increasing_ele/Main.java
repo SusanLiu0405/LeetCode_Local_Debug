c82577983159d8ca5348edb3c3a91b00f45a69c5
SusanLiu0405 <15301609750@163.com>
2023-03-20 22:26:42 -0400

    testcase passed

--- src/cisco/Num2016_Max_Diff_between_increasing_ele/Main.java
+++ src/cisco/Num2016_Max_Diff_between_increasing_ele/Main.java
@@ -2,9 +2,10 @@ package cisco.Num2016_Max_Diff_between_increasing_ele;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] test_array = {9,4,3,2};
+        System.out.println(maximumDifference(test_array));
     }
-    public int maximumDifference(int[] nums) {
+    public static int maximumDifference(int[] nums) {
         int diff = -1;
         for (int i = 1, min = nums[0]; i < nums.length; ++i) {
             if (nums[i] > min) {
