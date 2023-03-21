e9ca560b8c30bac055b6b0095f995db72ce3b5cc
SusanLiu0405 <15301609750@163.com>
2023-03-21 01:10:01 -0400

    testcase passed

--- src/cisco/word_puzzle/Main.java
+++ src/cisco/word_puzzle/Main.java
@@ -2,11 +2,19 @@ package cisco.word_puzzle;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        char[][] board = {{'C','A','T'},{'I','D','O'},{'N','O','M'}};
+        String word1 = "CAT";
+        String word2 = "TOM";
+        String word3 = "ADO";
+        String word4 = "MOM";
+        System.out.println(exist(board,word1));
+        System.out.println(exist(board,word2));
+        System.out.println(exist(board,word3));
+        System.out.println(exist(board,word4));
     }
     private static final int[][] DIRS = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-    public boolean exist(char[][] board, String word) {
-// corner case
+    public static boolean exist(char[][] board, String word) {
+        // corner case
         if (word == null || word.length() == 0) return false;
         boolean[][] visited = new boolean[board.length][board[0].length];
         for (int i = 0; i < board.length; i++) {
@@ -18,27 +26,28 @@ public class Main {
         }
         return false;
     }
-    private boolean dfs(char[][] board, String word, boolean[][] visited,
-                        int row, int col, int index) {
-// base case
+    private static boolean dfs(char[][] board, String word, boolean[][] visited, int row, int col, int index) {
+        // base case
         if (index == word.length()) return true;
-// check within bound
-        if (row < 0 || row >= board.length || col < 0 || col >=
-                board[0].length) return false;
-// check visited;
-        if (visited[row][col]) return false;
-// check match
-        if (board[row][col] != word.charAt(index)) return false;
-// mark visited
+        // check within bound
+        if (row < 0 || row >= board.length || col < 0 || col >= board[0].length)
+            return false;
+        // check visited;
+        if (visited[row][col])
+            return false;
+        // check match
+        if (board[row][col] != word.charAt(index))
+            return false;
+        // mark visited
         visited[row][col] = true;
-// do dfs
+        // do dfs
         for (int[] direction : DIRS) {
             int newRow = row + direction[0];
             int newCol = col + direction[1];
-            if(dfs(board, word, visited, newRow, newCol, index + 1)) return
-                    true;
+            if(dfs(board, word, visited, newRow, newCol, index + 1))
+                return true;
         }
-// backtracking
+        // backtracking
         visited[row][col] = false;
         return false;
     }
